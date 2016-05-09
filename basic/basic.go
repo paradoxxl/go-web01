2d540df3b7eb0a30e2d6ec065e527dcb6c175b5b
paradoxxl <edee>
2016-05-09 13:43:36 +0200

    some basic server functionality

---
+++ basic/basic.go
@@ -0,0 +1,20 @@
+package basic
+
+import (
+"fmt"
+"html"
+"log"
+"net/http"
+)
+
+func Start() {
+
+
+http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+})
+
+log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
