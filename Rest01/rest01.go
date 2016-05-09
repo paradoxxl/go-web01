2e90b8784e544c72eb4e11e30a045d964c47cdd1
paradoxxl <edee>
2016-05-09 13:51:14 +0200

    no message

--- Rest01/rest01.go
+++ Rest01/rest01.go
@@ -2,16 +2,33 @@ package rest01
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"html"
 	"log"
 	"net/http"
 )
 
 func Start() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/blubb",BlubbIndex)
+	router.HandleFunc("/blubb/{blubbId}",BlubbShow)
 
+	log.Fatal(http.ListenAndServe(":8080", router))
+
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func BlubbIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Blubb Index, %q", html.EscapeString(r.URL.Path))
+}
+
+func BlubbShow(w http.ResponseWriter, r *http.Request) {
+	vars:= mux.Vars(r)
+	Id := vars["blubbId"]
+	fmt.Fprintf(w, "Blubb: , %q", Id)
 }
