5fba591c80f809c05c99b6c855cc90bf45bd54aa
paradoxxl <edee>
2016-05-09 15:43:28 +0200

    tutorial end + queries

--- rest01/rest01.go
+++ rest01/rest01.go
@@ -1,34 +1,13 @@
 package rest01
 
 import (
-	"fmt"
-	"github.com/gorilla/mux"
-	"html"
 	"log"
 	"net/http"
 )
 
 func Start() {
 
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/blubb",BlubbIndex)
-	router.HandleFunc("/blubb/{blubbId}",BlubbShow)
+	router := NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8080", router))
-
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-}
-
-func BlubbIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Blubb Index, %q", html.EscapeString(r.URL.Path))
-}
-
-func BlubbShow(w http.ResponseWriter, r *http.Request) {
-	vars:= mux.Vars(r)
-	Id := vars["blubbId"]
-	fmt.Fprintf(w, "Blubb: , %q", Id)
 }
