efce3037c2eccf20e770a7228e8f61e8643b318e
paradoxxl <edee>
2016-05-09 13:07:41 +0200

    some basic server functionality

---
+++ Rest01/rest01.go
@@ -0,0 +1,17 @@
+package rest01
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func Start() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
