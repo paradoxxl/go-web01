2ba4a7059c00303ed41e5e9f471272d89f18eb7e
paradoxxl <edee>
2016-05-09 13:06:42 +0200

    some basic server functionality

--- Server.go
+++ Server.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/paradoxxl/go-web01/rest01"
+)
 
 func main() {
-	fmt.Println("hello Go")
+	fmt.Println("Trying to start Server")
+	rest01.Start()
 }
